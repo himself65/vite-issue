340769bb8a98dd9cbf8a1c5c7b42b6c206fc65c9
Alex Yang <himself65@outlook.com>
2024-01-01 08:07:08 -0600

    feat: case 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import * as vite from 'vite'
 import 'fake-package'
+import path from 'node:path'
 
 const server = await vite.createServer({
   appType: 'custom',
@@ -20,5 +21,32 @@ const server = await vite.createServer({
   const m = await server.ssrLoadModule('fake-package')
   console.log('m', m.default())
 }
-
+{
+  const m = await server.ssrLoadModule('./packages/fake-package')
+  console.log('m', m.default())
+}
+{
+  const m = await server.ssrLoadModule(path.resolve('./packages/fake-package'))
+  console.log('m', m.default())
+}
+{
+  const m = await server.ssrLoadModule(path.resolve('./packages/fake-package/index.js'))
+  console.log('m', m.default())
+}
+{
+  const m = await server.ssrLoadModule(path.resolve('./node_modules/fake-package'))
+  console.log('m', m.default())
+}
+{
+  const m = await server.ssrLoadModule(path.resolve('./node_modules/fake-package/index.js'))
+  console.log('m', m.default())
+}
+{
+  const m = await server.ssrLoadModule('./node_modules/fake-package/index.js')
+  console.log('m', m.default())
+}
+{
+  const m = await server.ssrLoadModule('/@fs/' + path.resolve('./node_modules/fake-package/index.js'))
+  console.log('m', m.default())
+}
 await server.close()
