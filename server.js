bcde4b90297685be2635527086c00683467ec253
Alex Yang <himself65@outlook.com>
2024-01-01 08:49:46 -0600

    fix: config

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ async function createServer () {
   const merged = vite.mergeConfig(config, {
     server: { middlewareMode: true }
   })
-  // HACK: Vite bug: TypeError [ERR_INVALID_ARG_TYPE]: The "path" argument must be of type string. Received function assetsInclude
-  merged.assetsInclude = null;
+  merged.assetsInclude = []
   const server = await vite.createServer(merged)
 
   await server.ssrLoadModule('./src/foo.ts')
